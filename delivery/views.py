c9f45a4cfbfc9fbb7605fd970dfe4bf5e53b1418
Andrey Matskevich <wedas018@mail.ru>
2022-10-31 15:03:44 +0300

    initial commit

--- delivery/views.py
+++ delivery/views.py
@@ -1,5 +1,22 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello user")
+    return render(request, 'index.html')
+
+
+def user(request, name, age):
+    return HttpResponse(f'<h1>Name {name}, age {age}</h1>')
+
+
+def about(request):
+    return HttpResponse("About")
+
+
+def contact(request):
+    return HttpResponseRedirect("/about")
+
+
+def details(request):
+    return HttpResponsePermanentRedirect("/")
--- deliveryProject/urls.py
+++ deliveryProject/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from delivery.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name="home"),
-    path('gamno/', index, name="gamno")
+    path('', index),
+    path("about/", about),
+    path("contact/", contact),
+    path("details/", details),
+    re_path(r"^user/(?P<name>\D+)/(?P<age>\d+)", user)
 ]
